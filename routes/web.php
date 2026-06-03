2d9a39ebd347604d5e3d3a111e988f5119246c33
nelsonraymond <nelsonraymondwu@gmail.com>
2026-06-03 14:44:49 +0700

    Auth

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,45 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\TicketController;
+use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\EventsController;
 use App\Http\Controllers\Admin\TransactionController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\EventController as EventAdminController;
 use App\Http\Controllers\Admin\PartnerController;
+
+
+// Rute Admin Area
+Route::prefix('admin')->name('admin.')->group(function () {
+
+    Route::get('/', function () {
+
+        if (auth()->check()) {
+            return redirect()->route('admin.dashboard');
+        }
+
+        return redirect()->route('admin.login');
+    });
+
+    Route::get('login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('login', [AuthController::class, 'login'])->name('login.post');
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::middleware(['auth', 'admin'])->group(function () {
+
+        Route::get('dashboard', [DashboardController::class, 'index'])
+            ->name('dashboard');
+
+        Route::resource('events', EventAdminController::class);
+        Route::resource('categories', CategoryController::class);
+        Route::resource('partners', PartnerController::class);
+
+        Route::get('transactions', [TransactionController::class, 'index'])
+            ->name('transactions.index');
+    });
+});
+
 
 // Rute User Area
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -21,19 +54,6 @@ Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
 Route::get('/profil', [HomeController::class, 'profil'])->name('profil');
 Route::get('/katalog', [HomeController::class, 'katalog'])->name('katalog');
 
-// Rute Admin Area
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    
-    Route::get('/events', [EventsController::class, 'index'])->name('events.index');
-    Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
-    Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
-});
-
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::resource('events', EventAdminController::class);
-    Route::resource('categories', CategoryController::class);
-    Route::resource('partners', PartnerController::class);
-    });
+Route::get('/login', function() {
+    return redirect()->route('admin.login');
+})->name('login');
